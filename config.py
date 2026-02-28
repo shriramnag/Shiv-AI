b97faf980930d7e53ac1319f916616e1986aa3be
Shriram Nag <Shriramjdp494.0.0@gmail.com>
2026-02-28 20:59:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,14 @@
-#
+import os
+
+# शिव एआई की मुख्य सेटिंग्स
+OWNER_NAME = "Shri Ram nag"
+AI_NAME = "Shiv AI"
+
+# एपीआई और मॉडल सेटिंग्स
+GROQ_API_KEY = "अपनी_की_यहाँ_डालें"
+MODEL_NAME = "llama3-70b-8192"
+
+# फाइलों के रास्ते (Paths)
+USER_DATA_PATH = "database/learning_data/userdata.txt"
+SYSTEM_CONTEXT_PATH = "database/learning_data/system_context.txt"
+
