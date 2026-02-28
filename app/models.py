c05efd5950ac6e347a698eaa1dce55a8769b0052
Shriram Nag <Shriramjdp494.0.0@gmail.com>
2026-02-28 21:17:52 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,10 @@
-#
+from pydantic import BaseModel
+
+class ChatRequest(BaseModel):
+    message: str
+    user_id: str = "default_user"
+
+class ChatResponse(BaseModel):
+    shiv_ai_says: str
+    owner: str = "Shri Ram nag"
+  
